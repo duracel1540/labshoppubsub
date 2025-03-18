c737527588c4a94a05e4f3699f6d6001fe327d44
duracel1540 <64642640+duracel1540@users.noreply.github.com>
2025-03-18 09:32:46 +0900

    commit

---
+++ delivery/src/main/java/labshoppubsub/domain/Delivery.java
@@ -0,0 +1,61 @@
+package labshoppubsub.domain;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import javax.persistence.*;
+import labshoppubsub.DeliveryApplication;
+import lombok.Data;
+
+@Entity
+@Table(name = "Delivery_table")
+@Data
+//<<< DDD / Aggregate Root
+public class Delivery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Long orderId;
+
+    public static DeliveryRepository repository() {
+        DeliveryRepository deliveryRepository = DeliveryApplication.applicationContext.getBean(
+            DeliveryRepository.class
+        );
+        return deliveryRepository;
+    }
+
+    //<<< Clean Arch / Port Method
+    public static void addDelivery(OrderPlaced orderPlaced) {
+        //implement business logic here:
+
+        /** Example 1:  new item 
+        Delivery delivery = new Delivery();
+        repository().save(delivery);
+
+        DeliveryAdded deliveryAdded = new DeliveryAdded(delivery);
+        deliveryAdded.publishAfterCommit();
+        */
+
+        /** Example 2:  finding and process
+        
+
+        repository().findById(orderPlaced.get???()).ifPresent(delivery->{
+            
+            delivery // do something
+            repository().save(delivery);
+
+            DeliveryAdded deliveryAdded = new DeliveryAdded(delivery);
+            deliveryAdded.publishAfterCommit();
+
+         });
+        */
+
+    }
+    //>>> Clean Arch / Port Method
+
+}
+//>>> DDD / Aggregate Root
